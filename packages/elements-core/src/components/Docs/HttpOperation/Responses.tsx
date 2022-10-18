a9156e8cbf2ca324806a3d7769c65e17afba65eb
Ellyx Christian <ellyx.christian@csgi.com>
2022-10-18 19:06:32 +0800

    Display schema in collapsible panel in body and response to increase performance for big schema the schema will be rendered when expanding and let the whole document render first instead of crash

--- packages/elements-core/src/components/Docs/HttpOperation/Responses.tsx
+++ packages/elements-core/src/components/Docs/HttpOperation/Responses.tsx
@@ -28,7 +28,7 @@ import { useSchemaInlineRefResolver } from '../../../context/InlineRefResolver';
 import { useOptionsCtx } from '../../../context/Options';
 import { getOriginalObject } from '../../../utils/ref-resolving/resolvedObject';
 import { MarkdownViewer } from '../../MarkdownViewer';
-import { SectionSubtitle, SectionTitle } from '../Sections';
+import { SectionSubtitle, SectionTitle, SubSectionPanel } from '../Sections';
 import { Parameters } from './Parameters';
 
 interface ResponseProps {
@@ -209,16 +209,20 @@ const Response = ({ response, onMediaTypeChange }: ResponseProps) => {
           </SectionSubtitle>
 
           {schema && (
-            <JsonSchemaViewer
-              schema={getOriginalObject(schema)}
-              resolveRef={refResolver}
-              maxRefDepth={maxRefDepth}
-              viewMode="read"
-              parentCrumbs={['responses', response.code]}
-              renderRootTreeLines
-              nodeHasChanged={nodeHasChanged}
-              renderExtensionAddon={renderExtensionAddon}
-            />
+            <SubSectionPanel title={(schema.title ?? '') + ' Schema'} defaultIsOpen={false} hasContent>
+              <Box pl={3} pr={3} pt={5}>
+                <JsonSchemaViewer
+                  schema={getOriginalObject(schema)}
+                  resolveRef={refResolver}
+                  maxRefDepth={maxRefDepth}
+                  viewMode="read"
+                  parentCrumbs={['responses', response.code]}
+                  renderRootTreeLines
+                  nodeHasChanged={nodeHasChanged}
+                  renderExtensionAddon={renderExtensionAddon}
+                />
+              </Box>
+            </SubSectionPanel>
           )}
         </>
       )}
